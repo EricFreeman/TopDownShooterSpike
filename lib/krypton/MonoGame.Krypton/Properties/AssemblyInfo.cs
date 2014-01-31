b8ff786af3f732ae9968684130eb3979b8a3cf6d
Adam Veldhousen <Adam.Veldhousen@extendhealth.com>
2014-01-30 20:33:38 -0700

    cleaned up krypton project. Retrofitting it to work with mono game.
    Conflicts:
    	rakefile

--- lib/krypton/MonoGame.Krypton/Properties/AssemblyInfo.cs
+++ lib/krypton/MonoGame.Krypton/Properties/AssemblyInfo.cs
@@ -5,24 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Krypton")]
-[assembly: AssemblyProduct("Krypton")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTitle("MonoGame.Krypton")]
+[assembly: AssemblyDescription("A port of Krypton to the MonoGame framework by Adam Veldhousen")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MonoGame.Krypton")]
+[assembly: AssemblyCopyright("Copyright 2011 Christopher Harris")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("0ce98116-fee5-4948-bdc1-edab3ed0d43e")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cbef7f17-ad7e-4df2-9a2c-877f285a4857")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
